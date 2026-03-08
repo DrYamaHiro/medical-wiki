ee41a5495db1e2429295cfd7516281c8e6d60e45
DrYamaHiro <dryamahiro@users.noreply.github.com>
2026-03-08 20:22:00 +0900

    Fix editUrl path: add medical-wiki/ subdirectory prefix

--- medical-wiki/docusaurus.config.js
+++ medical-wiki/docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/DrYamaHiro/medical-wiki/tree/master/',
+            'https://github.com/DrYamaHiro/medical-wiki/tree/master/medical-wiki/',
           showLastUpdateTime: false,
           showLastUpdateAuthor: false,
         },
